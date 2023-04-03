d0db5a1bb535ff9dd3c2290218cd8a944c32ae4f
juli <juli@polychainmonsters.com>
2023-04-03 20:31:21 +0200

    setup with wagmi

---
+++ src/providers/wagmiProvider/wagmiProvider.tsx
@@ -0,0 +1,49 @@
+import { ReactNode } from "react";
+import { WagmiConfig, configureChains, createClient } from "wagmi";
+import { polygon, polygonMumbai } from "@wagmi/core/chains";
+import { publicProvider } from "@wagmi/core/providers/public";
+import { infuraProvider } from "@wagmi/core/providers/infura";
+import { InjectedConnector } from "@wagmi/core/connectors/injected";
+import { WalletConnectConnector } from "@wagmi/core/connectors/walletConnect";
+import { CoinbaseWalletConnector } from "@wagmi/core/connectors/coinbaseWallet";
+import { WagmiProviderTypes } from "./wagmiProvider.types";
+
+const isProd = process.env.ENVIRONMENT == "PROD";
+const { chains, provider } = configureChains(
+  // what about dev and mainnet environments?
+  [isProd ? polygon : polygonMumbai],
+  [
+    infuraProvider({
+      apiKey: process.env.NEXT_PUBLIC_INFURA_API_KEY as string,
+      priority: 0,
+      stallTimeout: 1_000,
+    }),
+    publicProvider({ priority: 1 }),
+  ]
+);
+
+export const coinbaseConnector = new CoinbaseWalletConnector({
+  options: {
+    appName: "TheClassroom",
+    jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_MUMBAI_API_KEY}`,
+  },
+});
+
+export const injectedConnector = new InjectedConnector({ chains });
+
+export const walletConnectConnector = new WalletConnectConnector({
+  chains,
+  options: {
+    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string,
+  },
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: [injectedConnector, coinbaseConnector, walletConnectConnector],
+  provider,
+});
+
+export function WagmiProvider({ children }: WagmiProviderTypes) {
+  return <WagmiConfig client={wagmiClient}>{children}</WagmiConfig>;
+}
