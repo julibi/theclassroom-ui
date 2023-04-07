0565acdaf42d18c1faa35277fac3a216d357834c
juli <juli@polychainmonsters.com>
2023-04-07 17:12:10 +0200

    feat: functioning wallet connection modal

--- src/components/wallet-connection-modal/wallet-connection-modal.tsx
+++ src/components/wallet-connection-modal/wallet-connection-modal.tsx
@@ -1,27 +1,88 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import Image from "next/image";
+import { useSwitchNetwork } from "wagmi";
+import { utils } from "ethers";
+import { connect } from "@wagmi/core";
 import { Modal } from "../modal/modal";
 import styles from "./wallet-connection-modal.module.css";
-import { WalletConnectionModalProps } from "./wallet-connection-modal.types";
+import {
+  Connector,
+  WalletConnectionModalProps,
+} from "./wallet-connection-modal.types";
 import { Button } from "../button";
+import {
+  injectedConnector,
+  coinbaseConnector,
+  walletConnectConnector,
+} from "@/providers/wagmiProvider/wagmiProvider";
+import { connectors } from "@/constants";
 
 export const WalletConnectionModal = ({
   onClose,
 }: WalletConnectionModalProps) => {
-  const handleConnect = useCallback(() => {}, []);
+  const [connectError, setConnectError] = useState("");
+  const { error: switchError, switchNetwork } = useSwitchNetwork();
+  const networkSwitch = useCallback(async () => {
+    const switchTo = process.env.ENVIRONMENT === "DEV" ? 80001 : 137;
+    switchNetwork?.(switchTo);
+  }, [switchNetwork]);
+  const handleConnect = useCallback(
+    async (connector: Connector | undefined) => {
+      let selectedConnector;
+      if (connector === "Coinbase") {
+        selectedConnector = coinbaseConnector;
+      }
+      if (connector === "WalletConnect") {
+        selectedConnector = walletConnectConnector;
+      }
+      if (connector === "MetaMask") {
+        selectedConnector = injectedConnector;
+      }
+      // if (!selectedConnector) {
+      //   throw new Error("No connector found");
+      // }
+      try {
+        const result = await connect({
+          connector: selectedConnector,
+        });
+
+        if (utils.isAddress(result.account)) {
+          if (result?.chain?.unsupported) {
+            networkSwitch();
+          }
+          onClose?.();
+        }
+      } catch (e) {
+        console.log({ e });
+        setConnectError("Something went wrong.");
+      }
+    },
+    []
+  );
+
   return (
     <Modal onClose={onClose}>
       <div className={styles.content}>
-        <Button className={styles.connector} onClick={handleConnect}>
-          <Image
-            src="/MetaMask.png"
-            height={30}
-            width={30}
-            alt="MetaMask"
-            priority
-          />
-          <div>MetaMask</div>
-        </Button>
+        <h2>Connect Wallet</h2>
+        {connectors?.map((connector) => (
+          <Button
+            key={connector}
+            className={styles.connector}
+            onClick={() => handleConnect(connector)}
+          >
+            <Image
+              src={`/${connector}.png`}
+              height={25}
+              width={connector == "WalletConnect" ? 30 : 25}
+              alt={connector}
+              priority
+            />
+            <div>{connector}</div>
+          </Button>
+        ))}
+        <span className={styles.error}>
+          {connectError ?? switchError ?? " "}
+        </span>
       </div>
     </Modal>
   );
--- src/components/wallet-connection/wallet-connection.tsx
+++ src/components/wallet-connection/wallet-connection.tsx
@@ -1,45 +1,39 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
-
+import { useAccount, useEnsName } from "wagmi";
 import { Address } from "@wagmi/core";
-import { fetchEnsName, getAccount } from "@wagmi/core";
 import style from "./wallet-connection.module.css";
 import { truncateAddress } from "@/utils/truncateAddress";
 import { WalletConnectionModal } from "../wallet-connection-modal/wallet-connection-modal";
 import { Button } from "../button";
 
 export const WalletConnection = () => {
-  const { address } = getAccount();
+  const { address, isConnected } = useAccount();
+  const [name, setName] = useState<null | undefined | Address | string>(null);
+  const { data: ensName, error: ensError } = useEnsName({
+    address,
+  });
   const [isModalOpen, setIsOpenModal] = useState(false);
-
-  const handleConnect = async () => {
-    // const result = await connect({
-    //   connector: injectedConnector,
-    // });
-    // console.log({ result });
-  };
-
-  const [name, setName] = useState<Address | undefined>(undefined);
-
-  const fetchENS = useCallback(async () => {
-    if (!address) return;
-    try {
-      const bla = await fetchEnsName({ address });
-    } catch (e) {}
-  }, [name]);
 
   // because of this UI hydration error: https://nextjs.org/docs/messages/react-hydration-error
   useEffect(() => {
-    setName(address);
-    fetchENS();
-  }, [address]);
+    const nm =
+      ensName && !ensError
+        ? ensName
+        : address
+        ? truncateAddress(address)
+        : null;
 
+    nm && setName(nm);
+  }, [address, ensName, ensError]);
+  console.log({ name });
   return (
     <div className={style.walletConnection}>
-      {name ? (
-        <span>{truncateAddress(name)}</span>
-      ) : (
+      {name && isConnected && <span>{name}</span>}
+
+      {!isConnected && (
         <Button onClick={() => setIsOpenModal(true)}>Connect</Button>
       )}
+
       {isModalOpen && (
         <WalletConnectionModal onClose={() => setIsOpenModal(false)} />
       )}
