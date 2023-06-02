c41946bd00fc3750322f37929dd1f1d26ce65fda
juli <juli@polychainmonsters.com>
2023-06-02 17:17:58 +0200

    fix type issue Snippet

--- src/components/text-card/text-card.types.ts
+++ src/components/text-card/text-card.types.ts
@@ -1,5 +1,14 @@
-import { MutableRefObject } from "react";
-import { Snippet } from "../character-snippets/character-snippets.types";
+import { Address } from "wagmi";
+
+export type Snippet = {
+  character: number;
+  textIPFSHash: string;
+  translationIPFSHash: string;
+  tokenId: number;
+  writer: Address;
+  written: boolean;
+  writtenAt: number;
+};
 
 export type TextCardProps = {
   snippet: Snippet;
