7f920cf61a6e81f30d3996b8477b8657ac2e3609
juli <juli@polychainmonsters.com>
2023-06-22 19:53:51 +0200

    fix styling of profile avatar

--- src/components/avatar/account-avatar.tsx
+++ src/components/avatar/account-avatar.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 //@ts-ignore
 import Jazzicon from "@metamask/jazzicon";
 import { JazzIcon } from "./account-avatar.types";
+import styles from "./account-avatar.module.css";
 
 export const AccountAvatar = ({ address, size = 30 }: JazzIcon) => {
   const ref = useRef<HTMLDivElement>();
@@ -16,7 +17,7 @@ export const AccountAvatar = ({ address, size = 30 }: JazzIcon) => {
   }, [address, size]);
 
   return (
-    <div>
+    <div className={styles.avatar}>
       <div ref={ref as any} />
     </div>
   );
